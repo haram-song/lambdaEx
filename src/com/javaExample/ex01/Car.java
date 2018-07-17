89af31066c2c634308881d3d45eef50f96ee6421
Haram Song <thdgkfka098@naver.com>
2018-07-17 18:13:18 +0900

    주석 삭제(개인프로젝트이므로)

--- src/com/javaExample/ex01/Car.java
+++ src/com/javaExample/ex01/Car.java
@@ -1,29 +1,3 @@
-/**
- *  SMCP version 1.0
- *
- *  Copyright ⓒ 2018 kt corp. All rights reserved.
- *
- *  This is a proprietary software of kt corp, and you may not use this file except in
- *  compliance with license agreement with kt corp. Any redistribution or use of this
- *  software, with or without modification shall be strictly prohibited without prior written
- *  approval of kt corp, and the copyright notice above does not evidence any actual or
- *  intended publication of such software.
- *
- * @author kt
- * @since 2015.07.15
- * @version 1.0
- * @see 
- * @Copyright © 2015 By KT corp. All rights reserved.
- *
- * <pre>
- * << 개정이력(Modification Information) >>
- *  수정일               수정자                수정내용
- *  -------------        ----------       -------------------------
- *  2015.07.15           [성명]               최초생성            
- *
- *
- * </pre>
- */
 
 package com.javaExample.ex01;
 
@@ -33,9 +7,7 @@ package com.javaExample.ex01;
 public class Car implements Movable {
 
 
-/**
- *
- */
+
 	@Override
 	public void move(String str) {
 		// TODO Auto-generated method stub
